f4401068a48fb7fc195172b788a7c9e747c28f45
Morten Poul Jensen <mpj@signifly.com>
2019-09-10 12:10:45 +0200

    Mark test as incomplete

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -9,6 +9,8 @@ class ExampleTest extends TestCase
     /** @test */
     public function it_reaches_the_shopify_api()
     {
+        $this->markTestIncomplete();
+
         $shopify = $this->app->make(Shopify::class);
 
         $response = $shopify->products()->count();
