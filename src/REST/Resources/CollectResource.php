de2cdc0412d23cf55d3132270c33ff73814bc198
Morten Poul Jensen <mpj@foreno.dk>
2021-01-04 11:37:29 +0100

    feat: add delete method to collect resource

--- src/REST/Resources/CollectResource.php
+++ src/REST/Resources/CollectResource.php
@@ -4,4 +4,8 @@ namespace Signifly\Shopify\REST\Resources;
 
 class CollectResource extends ApiResource
 {
+    public function delete(): void
+    {
+        $this->shopify->deleteCollect($this->id);
+    }
 }
