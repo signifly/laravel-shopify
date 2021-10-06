89cfdb59a978e2b9c3a4d3dc330b344b05c2ba89
Phillip Raffnsøe <phillip@praffn.dk>
2021-10-06 11:21:38 +0200

    feat: add get/find/count/update methods for redirects

--- src/REST/Actions/ManagesOnlineStore.php
+++ src/REST/Actions/ManagesOnlineStore.php
@@ -2,6 +2,8 @@
 
 namespace Signifly\Shopify\REST\Actions;
 
+use Illuminate\Support\Collection;
+use Signifly\Shopify\REST\Cursor;
 use Signifly\Shopify\REST\Resources\ApiResource;
 use Signifly\Shopify\Shopify;
 
@@ -14,6 +16,31 @@ trait ManagesOnlineStore
             'path' => $path,
             'target' => $target,
         ]);
+    }
+
+    public function getRedirectsCount(array $params = []): int
+    {
+        return $this->getResourceCount('redirects', $params);
+    }
+
+    public function paginateRedirects(array $params = []): Cursor
+    {
+        return $this->cursor($this->getOrders($params));
+    }
+
+    public function getRedirects(array $params = []): Collection
+    {
+        return $this->getResources('redirects', $params);
+    }
+
+    public function getRedirect($redirectId): ApiResource
+    {
+        return $this->getResource('redirects', $redirectId);
+    }
+
+    public function updateRedirect($redirectId, $data): ApiResource
+    {
+        return $this->updateResource('redirects', $redirectId, $data);
     }
 
     public function deleteRedirect($redirectId): void
