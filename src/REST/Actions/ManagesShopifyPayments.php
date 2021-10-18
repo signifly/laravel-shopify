392ae1a79eb70c501f25fd06f7f9b4eccc3084b0
Ro Kleine Sonne <ro@signifly.com>
2021-10-18 13:00:26 +0200

    transactions

--- src/REST/Actions/ManagesShopifyPayments.php
+++ src/REST/Actions/ManagesShopifyPayments.php
@@ -5,6 +5,7 @@ namespace Signifly\Shopify\REST\Actions;
 use Illuminate\Support\Collection;
 use Signifly\Shopify\REST\Resources\BalanceResource;
 use Signifly\Shopify\REST\Resources\DisputeResource;
+use Signifly\Shopify\REST\Resources\TransactionResource;
 use Signifly\Shopify\Shopify;
 
 /** @mixin Shopify */
@@ -25,6 +26,26 @@ trait ManagesShopifyPayments
     public function getDispute($disputeId): DisputeResource
     {
         return $this->getResource('disputes', $disputeId, ['shopify_payments']);
+    }
+
+    public function createTransaction($orderId, array $data = [])
+    {
+        return $this->getResources('transactions', $data, ['orders', $orderId]);
+    }
+
+    public function getTransactions($orderId, array $params = []): Collection
+    {
+        return $this->getResources('transactions', $params, ['orders', $orderId]);
+    }
+
+    public function getTransaction($orderId): TransactionResource
+    {
+        return $this->getResource('transactions', ['orders', $orderId]);
+    }
+
+    public function getTransactionsCount(array $params = []): int
+    {
+        return $this->getResourceCount('transactions', $params);
     }
 
 }
