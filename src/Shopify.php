d9f980ad521e700cde2d3b19b17a14facdfd2fa0
eafarooqi <e_a_farooqi@hotmail.com>
2022-01-08 20:06:22 +0100

    fix: graphQL url without version

--- src/Shopify.php
+++ src/Shopify.php
@@ -75,7 +75,7 @@ class Shopify
 
     public function graphQl(): PendingRequest
     {
-        return Http::baseUrl($this->getBaseUrl().'/graphql.json')
+        return Http::baseUrl("https://{$this->domain}/admin/api/graphql.json")
             ->withHeaders(['X-Shopify-Access-Token' => $this->password]);
     }
 
